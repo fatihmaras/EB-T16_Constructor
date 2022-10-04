6cc078b91fbc5950ffca20ab75a01d884839a48e
fatihmaras <fatihmmaras@gmail.com>
2022-10-04 04:03:54 +0300

    Constructor Example

---
+++ EB-T16_Constructor/Kullanici.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EB_T16_Constructor
+{
+    public class Kullanici
+    {
+        private int KullaniciID { get; set; }
+        private string isim { get; set; }
+        private string Soyisim { get; set; }
+
+        private static int Maas { get; set; }
+
+
+        static Kullanici()
+        {
+            Maas = 2500;
+
+        }
+        public Kullanici(int xKullaniciID, string xisim, string xsoyisim, int xmaas)
+        {
+            KullaniciID = xKullaniciID;
+            isim = xisim;
+            Soyisim = xsoyisim;
+
+        }
+        public void BilgileriGoster()
+        {
+            Console.WriteLine("Kullanıcı Bilgileri");
+            Console.WriteLine("id : " + KullaniciID);
+            Console.WriteLine("isim :  " + isim);
+            Console.WriteLine("soyisim : " + Soyisim);
+            Console.WriteLine("Maaş : " + Maas);
+        }
+
+        public void ZamYap(int ZamMiktari)
+        {
+            Console.WriteLine("Kullanıcıya Zam Yapılıyor...");
+            Console.WriteLine("Şuanki Maaşı : " + (Maas + ZamMiktari));
+        }
+    }
+}
---
+++ EB-T16_Constructor/Program.cs
@@ -0,0 +1,27 @@
+﻿using EB_T16_Constructor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatickYapiciMethodKullanimi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Kullanici K1 = new Kullanici(1, "Fatih", "Maraş", 2500);
+
+            K1.BilgileriGoster();
+            K1.ZamYap(700);
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+
+}
